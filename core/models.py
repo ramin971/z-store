7ed8a45733ce9f6036086d3ef03d4aab0ab7cda2
ramin rahmani <ramin.rahmani971@gmail.com>
2024-02-26 19:07:44 +0330

    The models.py was designed

--- core/models.py
+++ core/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+
+class Product(models.Model):
+    pass
+
+class ProductImage(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Reaction(models.Model):
+    #like
+    #dislike
+    pass
+
+class Tag(models.Model):
+    pass
+
+class Descriprion(models.Model):
+    # video
+    # text
+    pass
+
+class Category(models.Model):
+    pass
+
+class Size(models.Model):
+    pass
+
+class Rating(models.Model):
+    pass
+
+class Coupon(models.Model):
+    pass
+
+class ReceiverInfo(models.Model):
+    #user
+    pass
+
+class Cart(models.Model):
+    pass
+
+class OrderItem(models.Model):
+    pass
--- store/settings.py
+++ store/settings.py
@@ -37,12 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # thirdparty app
+    'rest_framework',
+    'debug_toolbar',
+    # my app
     'core',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
